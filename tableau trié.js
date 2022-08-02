eaec6fba60d3f2b86891a913885612ff3047ecc0
nsab10 <107207894+nsab10@users.noreply.github.com>
2022-08-02 13:17:50 +0200

    Add files via upload

---
+++ tableau trié.js
@@ -0,0 +1,14 @@
+//Exercices1: trier le tableau par ordre croissant
+let tab;
+tab=[5,8,7,4,6,9,1,2,3];
+console.log('tableau original: ',tab);
+console.log('tableau trié: ',tab.sort());
+
+//exerice2: chercher le minimun et le maximum dans le tableau
+let tab;
+tab=[5,8,7,4,6,9,1,2,3];
+console.log("Min:",Math.min.apply(null,tab));
+console.log("Max:",Math.max.apply(null,tab));
+
+//exercice3: afficher un nombre impair d'un tableau
+
